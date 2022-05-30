06ce7afeb35d891b8c28391736348689bb85d1c9
Robert Pieter van Leeuwen <luivatra@gmail.com>
2022-05-30 12:17:04 +0200

    remove value condition

--- vesting/VestingState.py
+++ vesting/VestingState.py
@@ -77,7 +77,7 @@ class VestingState:
     def getContributionBox(self):
         contribBoxList = list(list(self._contributionBoxes.values()) + self.mempool.getUTXOsByTree(contributionTree))
         for box in contribBoxList:
-            if not self.mempool.isSpent(box["boxId"]) and box["value"] >= 22000000:
+            if not self.mempool.isSpent(box["boxId"]):
                 return box
     
     def getContributionBoxById(self, boxId):
