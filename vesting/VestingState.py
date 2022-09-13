d7262f345a13cd40c932b8e90f6c7b6998a67993
Robert Pieter van Leeuwen <luivatra@gmail.com>
2022-09-13 08:37:46 +0200

    removed the ergopos hardcode

--- vesting/VestingState.py
+++ vesting/VestingState.py
@@ -225,7 +225,7 @@ class VestingState:
             userOutput = appKit.mintToken(
                 value=int(5e5),
                 tokenId=proxyBox["boxId"],
-                tokenName=f"ErgoPOS {roundInfo['name']} Vesting Key",
+                tokenName=f"{roundInfo['name']} Vesting Key",
                 tokenDesc=f'{{"Vesting Round": "{roundInfo["name"]}", "Vesting start": "{datetime.fromtimestamp(proxyR4.getValue().apply(2)/1000)}", "Periods": {proxyR4.getValue().apply(1)}, "Period length": "{timedelta(milliseconds=proxyR4.getValue().apply(0)).days} day(s)", "Total vested": {(tokenAmount*10**(-1*self.roundInfo[proxyBox["assets"][1]["tokenId"]]["decimals"]))} }}',
                 mintAmount=1,
                 decimals=0,
