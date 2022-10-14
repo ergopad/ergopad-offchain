47e154b9b45faf85fd6fccbcaf93a6c2344d2c1a
Robert Pieter van Leeuwen <luivatra@gmail.com>
2022-10-14 14:48:54 +0200

    added await

--- vesting/__main__.py
+++ vesting/__main__.py
@@ -92,7 +92,7 @@ async def currentVestingState(config) -> VestingState:
             fetchedOracle = True
         else:
             logging.info("Timed out fetching oracle, trying again in 5 seconds")
-            asyncio.sleep(5000)
+            await asyncio.sleep(5000)
 
     offset = 0
     limit = 100
